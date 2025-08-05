f13f10a232b6ffe3057adde8d53e9e66bae4ecab
Zack-Fr <59012974+Zack-Fr@users.noreply.github.com>
2025-08-05 22:03:11 +0300

    fix/decoupling routing from redux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,12 @@ import ProductDetailPage from './pages/ProductDetailPage/ProductDetailPage';
 import Login from './pages/AuthPage/Login';
 import Register from './pages/AuthPage/Register';
 import CartPage from "./pages/CartPage/CartPage";
-import AdminRoutes from './routes/AdminRoutes';
-import CustomerRoutes from './routes/CustomerRoutes';
-
+import AddProduct from './pages/Admin/AddProduct/AddProduct';
+import Inventory from './pages/Admin/Inventory/Inventory';
+import SalesUsers from './pages/Admin/SalesUsers/SalesUsers'
+;
 import UserDashboardPage from "./pages/UserDashboardPage/UserDashboardPage";
-
+import ProtectedRoute from './routes/ProtectedRoutes';
 
 export default function App() {
 
@@ -25,14 +26,14 @@ export default function App() {
       <Route path="/login" element={<Login />}/>
       <Route path="/register" element={<Register />}/>
       <Route path="/cartpage" element={<CartPage />} />
-
-
-      {CustomerRoutes}
-      {AdminRoutes}
+    {/* admin routes */}
+    <>
+    <Route path="/admin/add-product" element={<ProtectedRoute roles={['admin']}><AddProduct /></ProtectedRoute>} />
+    <Route path="/admin/inventory" element={<ProtectedRoute roles={['admin']}><Inventory /></ProtectedRoute>} />
+    <Route path="/admin/sales-users" element={<ProtectedRoute roles={['admin']}><SalesUsers /></ProtectedRoute>} />
+    </>
 
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
-
-
--- frontend/src/routes/ProtectedRoutes.jsx
+++ frontend/src/routes/ProtectedRoutes.jsx
@@ -2,10 +2,10 @@ import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 export default function ProtectedRoute({ children, roles }) {
-const { user } = useSelector((state) => state.auth);
+// const { user } = useSelector((state) => state.auth);
 
-if (!user) return <Navigate to="/auth" />;
-if (roles && !roles.includes(user.role)) return <Navigate to="/" />;
+// if (!user) return <Navigate to="/auth" />;
+// if (roles && !roles.includes(user.role)) return <Navigate to="/" />;
 
 return children;
 }
